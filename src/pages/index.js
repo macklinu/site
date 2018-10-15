7db3624a3f989af57708007c1a3a0099e89bd601
Mackie Underdown <macklinu@users.noreply.github.com>
2018-10-14 20:52:23 -0700

    style(index): clean up article styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,9 +20,9 @@ function IndexWithData(data) {
         <h1 class="f5 f4-ns fw6 black">writings</h1>
         {getPostMetadata(data).map(({ title, date, slug }) => (
           <article className="bb b--black-10 mw7">
-            <Link to={slug} className="db pv2 no-underline black">
-              <h2 className="f6 f5-ns fw6 lh-title black-70">{title}</h2>
-              <h3 className="f6 fw4 mt0 black-60">{date}</h3>
+            <Link to={slug} className="db pv2 no-underline">
+              <h2 className="f6 f5-ns fw6 lh-title black">{title}</h2>
+              <h3 className="f6 fw4 mt0 black-70">{date}</h3>
             </Link>
           </article>
         ))}
