d81cc877bff04a90ff6f9c31307bee4ba08841d7
Mackie Underdown <macklinu@users.noreply.github.com>
2018-10-14 20:55:53 -0700

    refactor(index): extract ArticleLink component

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,23 +12,14 @@ function getPostMetadata(data) {
   }))
 }
 
-function IndexWithData(data) {
+function ArticleLink({ title, date, slug }) {
   return (
-    <Layout>
-      <Header />
-      <section className="ph4">
-        <h1 class="f5 f4-ns fw6 black">writings</h1>
-        {getPostMetadata(data).map(({ title, date, slug }) => (
-          <article className="bb b--black-10 mw7">
-            <Link to={slug} className="db pv2 no-underline">
-              <h2 className="f6 f5-ns fw6 lh-title black">{title}</h2>
-              <h3 className="f6 fw4 mt0 black-70">{date}</h3>
-            </Link>
-          </article>
-        ))}
-      </section>
-      <Footer />
-    </Layout>
+    <article className="bb b--black-10 mw7">
+      <Link to={slug} className="db pv2 no-underline">
+        <h2 className="f6 f5-ns fw6 lh-title black">{title}</h2>
+        <h3 className="f6 fw4 mt0 black-70">{date}</h3>
+      </Link>
+    </article>
   )
 }
 
@@ -55,7 +46,21 @@ export default function IndexPage() {
           }
         }
       `}
-      render={IndexWithData}
+      render={data => {
+        let posts = getPostMetadata(data)
+        return (
+          <Layout>
+            <Header />
+            <section className="ph4">
+              <h1 class="f5 f4-ns fw6 black">writings</h1>
+              {posts.map(post => (
+                <ArticleLink {...post} />
+              ))}
+            </section>
+            <Footer />
+          </Layout>
+        )
+      }}
     />
   )
 }
