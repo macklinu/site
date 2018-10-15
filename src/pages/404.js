e15af1a757d12dddf89fcdd47bc896959696efdf
Mackie Underdown <macklinu@users.noreply.github.com>
2018-10-14 20:52:04 -0700

    fix(404): add footer to page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function NotFoundPage() {
   return (
@@ -10,6 +11,7 @@ export default function NotFoundPage() {
         <h1 class="f5 f4-ns fw6 black">😅</h1>
         <h1 class="f5 f4-ns fw6 black">idk where that page is, sorry</h1>
       </section>
+      <Footer />
     </Layout>
   )
 }
