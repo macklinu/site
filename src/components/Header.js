f350ea0e773c5e9b352c6ebbf9639e70b2a102ab
Mackie Underdown <macklinu@users.noreply.github.com>
2018-12-07 23:03:12 -0800

    fix(Header): use className instead of class

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,13 @@ export default function Header() {
   return (
     <header className="pa4">
       <Link to="/" className="link dib">
-        <img src={mackie} class="br-100 ba b--black-10 h3 w3" alt="avatar" />
-        <h1 class="f5 f4-ns fw6 black">mackie</h1>
-        <h2 class="f6 gray fw2">engineer, musician, artist</h2>
+        <img
+          src={mackie}
+          className="br-100 ba b--black-10 h3 w3"
+          alt="avatar"
+        />
+        <h1 className="f5 f4-ns fw6 black">mackie</h1>
+        <h2 className="f6 gray fw2">engineer, musician, artist</h2>
       </Link>
     </header>
   )
