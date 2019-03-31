bb77b89948d04ab69685bf9807ee1dba615923a2
Mackie Underdown <macklinu@users.noreply.github.com>
2019-03-31 13:28:06 -0700

    Revert ":recycle: Switch to Gatsby browser API for theme providing"
    This reverts commit f42f76eb12aff6310c002463aa15f82a96a95598.

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { ThemeProvider } from 'styled-components'
-
-import theme from './src/components/theme'
-
-export const wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={theme}>{element}</ThemeProvider>
-)
