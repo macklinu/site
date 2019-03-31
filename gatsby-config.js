fbc411d660140e85a013df2ebf463f6fe127ee71
Mackie Underdown <macklinu@users.noreply.github.com>
2019-03-31 13:09:46 -0700

    :art: Update manifest color to match site theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,8 +48,8 @@ module.exports = {
         name: 'mackie.world',
         short_name: 'mackie.world',
         start_url: '/',
-        background_color: '#ff41b4',
-        theme_color: '#ff41b4',
+        background_color: '#F8EAF4',
+        theme_color: '#F8EAF4',
         display: 'minimal-ui',
         icon: 'src/images/globe.png', // This path is relative to the root of the site.
       },
