2b33540bc15f321fc4c88010dab138a359cfd401
Macklin Underdown <macklinu@users.noreply.github.com>
2019-08-13 09:27:05 -0700

    Integrate my theme for the redesign

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,53 +5,7 @@ module.exports = {
       'my name is mackie. i am a software engineer and musician. welcome to my site.',
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        gfm: true,
-        plugins: [
-          {
-            resolve: '@weknow/gatsby-remark-twitter',
-          },
-          {
-            resolve: 'gatsby-remark-emoji',
-          },
-          {
-            resolve: 'gatsby-remark-component',
-            options: {
-              components: ['banner'],
-            },
-          },
-          {
-            resolve: 'gatsby-remark-images',
-          },
-        ],
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'posts',
-        path: `${__dirname}/src/pages/posts/`,
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'notes',
-        path: `${__dirname}/src/pages/notes/`,
-      },
-    },
-    'gatsby-transformer-yaml',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/data/`,
-      },
-    },
+    'gatsby-theme-macklinu-portfolio',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
