b798fb2210f04b343d54bbbb5d6c9d0cbdbf5162
Mackie Underdown <macklinu@users.noreply.github.com>
2020-10-20 16:34:34 -0700

    feat: add new page styles

---
+++ src/components.js
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import { Link as GatsbyLink } from 'gatsby'
+import tw, { styled } from 'twin.macro'
+
+export const Link = ({
+  children,
+  to,
+  activeClassName,
+  partiallyActive,
+  ...other
+}) => {
+  const internal = /^\/(?!\/)/.test(to)
+
+  if (internal) {
+    return (
+      <GatsbyLink
+        to={to}
+        activeClassName={activeClassName}
+        partiallyActive={partiallyActive}
+        {...other}
+      >
+        {children}
+      </GatsbyLink>
+    )
+  }
+  return (
+    <a href={to} {...other}>
+      {children}
+    </a>
+  )
+}
+
+export const NavLink = tw(
+  Link
+)`text-lg font-bold hover:underline p-2 transition duration-500 ease-in-out`
+
+export const Container = tw.div`max-w-3xl mx-auto`
--- src/page-layout.js
+++ src/page-layout.js
@@ -1,23 +1,46 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import { GlobalStyles } from 'twin.macro'
+import tw, { GlobalStyles } from 'twin.macro'
+import { Container, NavLink } from './components'
 import face from './mackie-face.png'
 
-export const PageLayout = ({ children }) => (
-  <>
-    <GlobalStyles />
-    <div tw='antialiased py-4 max-w-3xl mx-auto'>
-      <header>
-        <Link to='/'>
-          <div tw='inline-flex flex-row items-center'>
-            <img alt="mackie's face" tw='h-8 w-8' src={face} />
-            <div tw='px-1' />
-            <h1 tw='font-black text-2xl'>mackie</h1>
-          </div>
-        </Link>
-      </header>
-      <div tw='py-2' />
-      {children}
+const Header = () => (
+  <header tw='flex flex-row py-4'>
+    <NavLink to='/'>
+      <div tw='flex flex-row items-center'>
+        <img alt="mackie's face" tw='h-8 w-8' src={face} />
+        <div tw='px-1' />
+        <h1 tw='font-bold text-2xl'>mackie</h1>
+      </div>
+    </NavLink>
+    <div tw='mx-auto' />
+    <div tw='flex flex-row space-x-4 items-center'>
+      <NavLink to='/about'>about</NavLink>
     </div>
-  </>
+  </header>
+)
+
+const Footer = () => (
+  <footer tw='py-8 bg-purple-200'>
+    <Container tw='flex flex-row items-center'>
+      <div tw='mx-auto' />
+      <div tw='flex flex-row space-x-4 justify-end'>
+        <NavLink to='https://github.com/macklinu'>github</NavLink>
+        <NavLink to='https://twitter.com/macklinu'>twitter</NavLink>
+        <NavLink to='https://linkedin.com/in/macklinu'>linkedin</NavLink>
+      </div>
+    </Container>
+  </footer>
+)
+
+export const PageLayout = ({ children }) => (
+  <div tw='h-screen antialiased'>
+    <GlobalStyles />
+    <div>
+      <Container>
+        <Header />
+      </Container>
+      {children}
+      <Footer />
+    </div>
+  </div>
 )
