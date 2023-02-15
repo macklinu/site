32c9198f1e6283c5fdff7d9470926a4aea09e4b5
mackie <macklinu@users.noreply.github.com>
2023-02-15 13:48:40 -0500

    Parse post dates as ISO
    This fixes an issue in development where the dates would be off by one day. I haven't seen this issue in production, but hoping this will be more explicit and prevent any potential date string parsing issues.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,12 @@
 import { defineCollection, z } from 'astro:content'
+import { DateTime } from 'luxon'
 
 export const collections = {
   posts: defineCollection({
     schema: z.object({
       title: z.string(),
       description: z.string(),
-      date: z.coerce.date(),
+      date: z.string().transform((s) => DateTime.fromISO(s).toJSDate()),
       published: z.coerce.boolean().optional().default(true),
     }),
   }),
