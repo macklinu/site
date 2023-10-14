436dd97e030ffb07f561c72aed9039286cc025fb
Mackie Underdown <macklinu@users.noreply.github.com>
2023-10-14 09:58:19 -0400

    Generate Open Graph images (#33)
    Resolves #21

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://mackie.world',
+  integrations: [tailwind(), react()],
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
       theme: 'github-dark',
+    },
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ['@resvg/resvg-js'],
     },
   },
 })
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+namespace React {
+  interface Attributes {
+    tw?: string
+  }
+}
